86a960bec880b1cc1c4a806bf9fc671282f75e6f
Maria Pandele <maria.pandele33@gmail.com>
2019-03-31 21:47:41 +0300

    Added simple user

---
+++ src/com/mapa/model/User.java
@@ -0,0 +1,42 @@
+package com.mapa.model;
+
+import java.util.Date;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String emailAddress;
+    private Date birthDay;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
+}
