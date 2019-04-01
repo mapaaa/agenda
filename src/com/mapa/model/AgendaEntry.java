bcec56947b7eddd4eae529cb2ae6ad4923e0718b
Maria Pandele <maria.pandele33@gmail.com>
2019-04-01 13:39:33 +0300

    Added simple AgendaEntr

--- src/com/mapa/model/AgendaEntry.java
+++ src/com/mapa/model/AgendaEntry.java
@@ -3,6 +3,10 @@ package com.mapa.model;
 public abstract class AgendaEntry {
     private String name;
 
+    AgendaEntry(String name) {
+        this.name = name;
+    }
+
     public String getName() {
         return name;
     }
