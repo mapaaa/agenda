07f32cda3b2f01c738bc6e9f72f224f4d8dff4d9
Maria Pandele <maria.pandele33@gmail.com>
2019-04-01 21:43:56 +0300

    Added user demo

--- src/com/mapa/Main.java
+++ src/com/mapa/Main.java
@@ -11,6 +11,7 @@ public class Main {
     public static void main(String[] args) {
         User user = new User(123, "Jane", "Doe", "jane.dow@aaa.com", new Date());
         var agendaManager = AgendaManager.getInstance();
+        var accountManager = AccountManager.getInstance(user);
         //TODO: maybe builders?
         //TODO: id manager
         agendaManager.addEvent(1, "event", "event description", new Date(), "11 Academiei", false);
@@ -30,5 +31,13 @@ public class Main {
         System.out.println("Let's complete task with id = 5");
         agendaManager.completeTask(5);
         System.out.println(agendaManager.getTaskById(5));
+
+        System.out.println("\n");
+
+        System.out.println("Current user: " + accountManager.getUser());
+        user.setEmailAddress("notAnEmail@aa@bb.com");
+        accountManager.modifyUser(user);
+        System.out.println("User email modified");
+        System.out.println("Current userL " + accountManager.getUser());
     }
 }
