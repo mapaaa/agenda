16bcf8db01c361e105d92ecab3b2dcadbd566f66
Maria Pandele <maria.pandele33@gmail.com>
2019-04-27 22:15:38 +0300

    Added register

--- src/com/mapa/Main.java
+++ src/com/mapa/Main.java
@@ -1,12 +1,12 @@
 package com.mapa;
 
 import com.mapa.service.AccountManager;
-import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
         var accountManager = AccountManager.getInstance();
-        accountManager.Login();
+      //  accountManager.Login();
+        accountManager.Register();
     }
 }
--- src/com/mapa/service/AccountManager.java
+++ src/com/mapa/service/AccountManager.java
@@ -4,15 +4,21 @@ import com.mapa.model.User;
 
 import javax.swing.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Optional;
 
 public class AccountManager {
     private static AccountManager instance;
-    private  User user;
+    private User user;
 
     private AccountManager(){ }
 
@@ -32,7 +38,7 @@ public class AccountManager {
     }
 
     // Performs username and password verification and returns corresponding user id
-    public void Login() throws FileNotFoundException {
+    public void Login() {
         Optional<Integer> userId = Optional.empty();
         int attempts = 3;
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
@@ -44,25 +50,16 @@ public class AccountManager {
                 --attempts;
 
                 System.out.println("Enter email address: ");
-                String userName = buffer.readLine();
-                System.out.println("Enter password");
+                String emailAddress = buffer.readLine();
+                System.out.println("Enter password: ");
                 JPasswordField pf = new JPasswordField();
                 String password = JOptionPane.showConfirmDialog(null, pf, "Enter password: ", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf.getPassword()) : "";
-
-                MessageDigest md5 = MessageDigest.getInstance("md5");
-                md5.update(password.getBytes());
-
-                byte[] passwordBytes = md5.digest();
-                StringBuilder stringBuilder = new StringBuilder();
-                for (byte passwordByte : passwordBytes) {
-                    stringBuilder.append(Integer.toString((passwordByte & 0xff) + 0x100, 16).substring(1));
-                }
-                String passwordHash = stringBuilder.toString();
+                String passwordHash = md5HashPassword(password);
                 BufferedReader credentialsBuffer = new BufferedReader(new FileReader("data/Credentials.txt"));
                 String line;
                 while ((line = credentialsBuffer.readLine()) != null) {
                     String[] info = line.split("\\s+");
-                    if (info[0].equals(userName) && info[1].equals(passwordHash)) {
+                    if (info[0].equals(emailAddress) && info[1].equals(passwordHash)) {
                         foundCredentials = true;
                         userId = Optional.of(Integer.parseInt(info[2]));
                         break;
@@ -76,22 +73,120 @@ public class AccountManager {
             if (attempts == 0) {
                 System.out.println("Sorry...too many login attempts");
             }
-        } catch (IOException | NoSuchAlgorithmException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         if (userId.isPresent()) {
             User user = getUserById(userId.get());
-            System.out.println("Login successful");
-            System.out.println("Welcome back " + user.getFirstName());
+            if (user != null) {
+                System.out.println("Login successful");
+                System.out.println("Welcome back " + user.getFirstName());
+            }
          }
     }
 
-    private User getUserById(Integer userId) throws FileNotFoundException {
-        BufferedReader usersBuffer = new BufferedReader(new FileReader("data/Users.txt"));
-        String line;
+    // Register new user
+    // TODO: validations
+    public void Register() {
+        System.out.println("Please complete the following register form: ");
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            boolean formCompleted = false;
+            while(!formCompleted) {
+                System.out.println("Enter first name: ");
+                String firstName = buffer.readLine();
+                System.out.println("Enter last name: ");
+                String lastName = buffer.readLine();
+                System.out.println("Enter email address: ");
+                String emailAddress = buffer.readLine();
+                System.out.println("Enter password: ");
+                JPasswordField pf1 = new JPasswordField();
+                String password1 = JOptionPane.showConfirmDialog(null, pf1, "Enter password: ", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf1.getPassword()) : "";
+                System.out.println("Confirm password: ");
+                JPasswordField pf2 = new JPasswordField();
+                String password2 = JOptionPane.showConfirmDialog(null, pf2, "Confirm password: ", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf2.getPassword()) : "";
+                if (!password1.equals(password2)) {
+                    System.out.println("Passwords don't match. Try again");
+                    continue;
+                }
+                String passwordHash = md5HashPassword(password1);
+                System.out.println("Enter birth date (dd/mm/yyyy format): ");
+                Date birthDay = new SimpleDateFormat("dd/MM/yyyy").parse(buffer.readLine());
+
+                var user = createNewUser(firstName, lastName, emailAddress, birthDay);
+                formCompleted = true;
+
+                SaveData(user, passwordHash);
+            }
+        }
+        catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Register successful! Redirecting to login...");
+        Login();
+    }
+
+    private void SaveData(User user, String passwordHash) {
+        try {
+            BufferedWriter usersBuffer = new BufferedWriter(new FileWriter("data/Users.txt", true));
+            BufferedWriter credentialsBuffer = new BufferedWriter(new FileWriter("data/Credentials.txt", true));
+
+            String birthDay = new SimpleDateFormat("dd/MM/yyyy").format(user.getBirthDay());
+            String userInfo = user.getId() + " " + user.getFirstName() + " " + user.getLastName() + " " + user.getEmailAddress() + " " + birthDay;
+            String credentialsInfo = user.getEmailAddress() + " " + passwordHash + " " + user.getId();
+
+            usersBuffer.newLine();
+            usersBuffer.append(userInfo);
+
+            credentialsBuffer.newLine();
+            credentialsBuffer.append(credentialsInfo);
+            credentialsBuffer.close();
+            usersBuffer.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String md5HashPassword(String password) {
+        String passwordHash = "";
+        try {
+            MessageDigest md5 = MessageDigest.getInstance("md5");
+            md5.update(password.getBytes());
+
+            byte[] passwordBytes = md5.digest();
+            StringBuilder stringBuilder = new StringBuilder();
+            for (byte passwordByte : passwordBytes) {
+                stringBuilder.append(Integer.toString((passwordByte & 0xff) + 0x100, 16).substring(1));
+            }
+            passwordHash = stringBuilder.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return passwordHash;
+    }
+
+    private User createNewUser(String firstName, String lastName, String emailAddress, Date birthDay) {
+        User newUser = null;
+        try {
+            Path path = Paths.get("data/Credentials.txt");
+            long userId = Files.lines(path).count() + 1;
+            System.out.println(userId);
+            newUser = new User((int)userId, firstName, lastName, emailAddress, birthDay);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return newUser;
+    }
+
+    private User getUserById(Integer userId) {
         User user = null;
         try {
+            BufferedReader usersBuffer = new BufferedReader(new FileReader("data/Users.txt"));
+            String line;
             while ((line = usersBuffer.readLine()) != null) {
                 String[] info = line.split("\\s+");
                 if (userId.compareTo(Integer.parseInt(info[0])) == 0) {
