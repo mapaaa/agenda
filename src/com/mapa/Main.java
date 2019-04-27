4699cf13c941b14d4222716f48b036a1460a73a3
Maria Pandele <maria.pandele33@gmail.com>
2019-04-27 21:15:21 +0300

    Added Login and some data files to demo this. Deleted the old demo

--- src/com/mapa/Main.java
+++ src/com/mapa/Main.java
@@ -1,43 +1,12 @@
 package com.mapa;
 
-import com.mapa.model.User;
 import com.mapa.service.AccountManager;
-import com.mapa.service.AgendaManager;
-
-import java.util.Date;
+import java.io.FileNotFoundException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        User user = new User(123, "Jane", "Doe", "jane.dow@aaa.com", new Date());
-        var agendaManager = AgendaManager.getInstance();
-        var accountManager = AccountManager.getInstance(user);
-        //TODO: maybe builders?
-        //TODO: id manager
-        agendaManager.addEvent(1, "event", "event description", new Date(), "11 Academiei", false);
-        agendaManager.addEvent(2, "all day event", "all day event description", new Date(), "11 Academiei", true);
-        agendaManager.addNote(3, "Shopping", "Tomatoes, Potatoes");
-        agendaManager.addReminder(4, "Submit homework", new Date(), false);
-        agendaManager.addTask(5,"Homework", "Details in Proiect.txt");
-
-        var allEntries = agendaManager.getAllEntries();
-        System.out.println(allEntries.size());  // 5
-        for (var entry : allEntries) {
-            System.out.println(entry);
-        }
-
-        System.out.println("\n");
-
-        System.out.println("Let's complete task with id = 5");
-        agendaManager.completeTask(5);
-        System.out.println(agendaManager.getTaskById(5));
-
-        System.out.println("\n");
-
-        System.out.println("Current user: " + accountManager.getUser());
-        user.setEmailAddress("notAnEmail@aa@bb.com");
-        accountManager.modifyUser(user);
-        System.out.println("User email modified");
-        System.out.println("Current userL " + accountManager.getUser());
+    public static void main(String[] args) throws FileNotFoundException {
+        var accountManager = AccountManager.getInstance();
+        accountManager.Login();
     }
 }
--- src/com/mapa/service/AccountManager.java
+++ src/com/mapa/service/AccountManager.java
@@ -2,18 +2,23 @@ package com.mapa.service;
 
 import com.mapa.model.User;
 
+import javax.swing.*;
+import java.io.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Optional;
+
 public class AccountManager {
     private static AccountManager instance;
     private  User user;
 
-    //TODO: authentication
-    private AccountManager(User user){
-        this.user = user;
-    }
+    private AccountManager(){ }
 
-    public static AccountManager getInstance(User user) {
+    public static AccountManager getInstance() {
         if (instance == null) {
-            return instance = new AccountManager(user);
+            return instance = new AccountManager();
         }
         return instance;
     }
@@ -24,5 +29,85 @@ public class AccountManager {
 
     public void modifyUser(User newUser) {
         user = newUser;
+    }
+
+    // Performs username and password verification and returns corresponding user id
+    public void Login() throws FileNotFoundException {
+        Optional<Integer> userId = Optional.empty();
+        int attempts = 3;
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Login");
+        boolean foundCredentials = false;
+        try {
+            while (attempts != 0 && !foundCredentials) {
+                --attempts;
+
+                System.out.println("Enter email address: ");
+                String userName = buffer.readLine();
+                System.out.println("Enter password");
+                JPasswordField pf = new JPasswordField();
+                String password = JOptionPane.showConfirmDialog(null, pf, "Enter password: ", JOptionPane.OK_CANCEL_OPTION, JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION ? new String(pf.getPassword()) : "";
+
+                MessageDigest md5 = MessageDigest.getInstance("md5");
+                md5.update(password.getBytes());
+
+                byte[] passwordBytes = md5.digest();
+                StringBuilder stringBuilder = new StringBuilder();
+                for (byte passwordByte : passwordBytes) {
+                    stringBuilder.append(Integer.toString((passwordByte & 0xff) + 0x100, 16).substring(1));
+                }
+                String passwordHash = stringBuilder.toString();
+                BufferedReader credentialsBuffer = new BufferedReader(new FileReader("data/Credentials.txt"));
+                String line;
+                while ((line = credentialsBuffer.readLine()) != null) {
+                    String[] info = line.split("\\s+");
+                    if (info[0].equals(userName) && info[1].equals(passwordHash)) {
+                        foundCredentials = true;
+                        userId = Optional.of(Integer.parseInt(info[2]));
+                        break;
+                    }
+                }
+                if (!foundCredentials) {
+                    System.out.println("Wrong username or password. Please try again.");
+                }
+
+            }
+            if (attempts == 0) {
+                System.out.println("Sorry...too many login attempts");
+            }
+        } catch (IOException | NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        if (userId.isPresent()) {
+            User user = getUserById(userId.get());
+            System.out.println("Login successful");
+            System.out.println("Welcome back " + user.getFirstName());
+         }
+    }
+
+    private User getUserById(Integer userId) throws FileNotFoundException {
+        BufferedReader usersBuffer = new BufferedReader(new FileReader("data/Users.txt"));
+        String line;
+        User user = null;
+        try {
+            while ((line = usersBuffer.readLine()) != null) {
+                String[] info = line.split("\\s+");
+                if (userId.compareTo(Integer.parseInt(info[0])) == 0) {
+                    user = new User(
+                            userId,
+                            info[1],
+                            info[2],
+                            info[3],
+                            new SimpleDateFormat("dd/MM/yyyy").parse(info[4]));
+                    break;
+                }
+            }
+        }
+        catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
